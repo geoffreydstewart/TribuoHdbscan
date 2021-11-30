7d371f9d9c66d980c2922d83acbff3e4cad482cf
Geoffrey Stewart <geoffreydstewart@hotmail.com>
2021-11-29 22:04:38 -0800

    correctness tests after PR review changes, tests with latest jar, all good

---
+++ other/copy_jars.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#
+# copy jars
+#
+
+cp /Users/gstewart/.m2/repository/org/tribuo/tribuo-clustering-hdbscan/4.2.0-SNAPSHOT/tribuo-clustering-hdbscan-4.2.0-SNAPSHOT-jar-with-dependencies.jar /data/src/other/gsc/mscs/TribuoHdbscan/jars
