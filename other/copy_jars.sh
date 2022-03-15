d4adabcfb2372d63f2e7fd0931a145a8d99f6931
Geoffrey Stewart <geoffreydstewart@hotmail.com>
2022-03-15 15:33:04 -0700

    Verify the proposed prediction changes that will come in version 4.3

--- other/copy_jars.sh
+++ other/copy_jars.sh
@@ -3,4 +3,4 @@
 # copy jars
 #
 
-cp /Users/gstewart/.m2/repository/org/tribuo/tribuo-clustering-hdbscan/4.2.0-SNAPSHOT/tribuo-clustering-hdbscan-4.2.0-SNAPSHOT-jar-with-dependencies.jar /data/src/other/gsc/mscs/TribuoHdbscan/jars
+cp /Users/gstewart/.m2/repository/org/tribuo/tribuo-clustering-hdbscan/4.3.0-SNAPSHOT/tribuo-clustering-hdbscan-4.3.0-SNAPSHOT-jar-with-dependencies.jar /data/src/other/gsc/mscs/TribuoHdbscan/jars
